b739812cbc8a1a9896d921a0c027673b46fd4fd3
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-09-16 18:09:13 -0500

    added array of question/answers

--- app.js
+++ app.js
@@ -1,35 +1,98 @@
-/**
- * Example store structure
- */
+/* global $ */
 'use strict';
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
+
+const pageStore = {
+  // welcomePage: true
+  welcomeMessage: '<h1>Welcome to know your Colors</h1><button class="startBtn">Start Quiz</button>',
+  welcomePage: true,
+  quizPage: false
 };
+
+const questions = [
+  {
+    question: 'What color mixing makes  Orange?',
+    answers: [
+      'A. Red and Yellow',
+      'B. Pink and Green',
+      'C. Green and Yellow',
+    ],
+    correctAnswer: 'A'
+  },
+  {
+    question: 'What color mixing makes Purple?',
+    answers: [
+      'A. Purple and Yellow',
+      'B. Blue and Red',
+      'C. Pink and Yellow',
+    ],
+    correctAnswer: 'B'
+  },
+  {
+    question: 'What color mixing makes Green?',
+    answers: [
+      'A. White and Yellow',
+      'B. Black and Red',
+      'C. Blue and Yellow',
+    ],
+    correctAnswer: 'C'
+  },
+  {
+    question: 'What color mixing makes Gray?',
+    answers: [
+      'A. White and Brown',
+      'B. White and Black',
+      'C. Black and Gray',
+    ],
+    correctAnswer: 'B'
+  },
+  {
+    question: 'What color mixing makes Pink?',
+    answers: [
+      'A. White and Orange',
+      'B. White and Yellow',
+      'C. White and Red',
+    ],
+    correctAnswer: 'C'
+  }
+]
+
+// Welcome function to render welcome html
+const welcome = () => {
+  render();
+  startHandler();
+};
+
+// start button handler
+const startHandler = () => {
+  $('.startBtn').click(() => {
+    pageStore.welcomePage = false;
+    pageStore.quizPage = true;
+    render();
+  });
+};
+
+// a function for the questions and answers that utilizes a template string
+const quiz = () => {
+
+};
+//! answers must be in a form
+
+
+
+const render = () => {
+  if (pageStore.welcomePage) {
+    $('main').html(pageStore.welcomeMessage);
+  } else {
+    $('main').empty();
+  }
+};
+
+const main = () => {
+  welcome();
+};
+
+$(main);
+
 
 /**
  *
@@ -48,12 +111,11 @@ const store = {
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
-  // These functions return HTML templates
+// These functions return HTML templates
 
 /********** RENDER FUNCTION(S) **********/
 
-  // This function conditionally replaces the contents of the <main> tag based on the state of the store
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
 
 /********** EVENT HANDLER FUNCTIONS **********/
-
-  // These functions handle events (submit, click, etc)
+// These functions handle events (submit, click, etc)
